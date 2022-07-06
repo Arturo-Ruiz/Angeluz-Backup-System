197fdbe84c97b1107cfeeb551746d450562e09f9
Arturo Ruiz <Gregory.Arturo.Ruiz@gmail.com>
2022-07-06 13:59:13 -0400

    add sweet alert and successful upload page

---
+++ successful_upload.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon.png">
+    <link rel="icon" type="image/png" href="img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Angeluz Backup System</title>
+    <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+    <!--    Icons     -->
+    <link href="vendor/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body class="">
+    <div class="wrapper ">
+        <div class="sidebar" data-color="azure" data-background-color="white">
+            <div class="logo">
+
+            </div>
+            <div class="sidebar-wrapper">
+                <ul class="nav">
+                    <li class="nav-item active  ">
+                        <a class="nav-link" href="#">
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                            <p>Panel de control</p>
+                        </a>
+                    </li>
+                    <li class="nav-item active-pro ">
+                        <a class="nav-link" target="_blank" href="https://api.whatsapp.com/send?phone=+584128620300&text=Hola%20Henry!,%20El%20sistema%20necesita%20soporte%20t%C3%A9cnico!">
+                            <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                            <p>Soporte Tecnico</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="main-panel">
+            <!-- Navbar -->
+            <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+                <div class="container-fluid">
+                    <div class="navbar-wrapper">
+                        <a class="navbar-brand" href="#">Panel de control</a>
+                    </div>
+                </div>
+            </nav>
+            <!-- End Navbar -->
+            <div class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <a href="upload.php">
+                                <div class="card card-stats">
+                                    &nbsp;
+                                    <div class="card-header card-header-success card-header-icon">
+                                        <div class="card-icon">
+                                            <i class="fa fa-cloud-upload" aria-hidden="true"></i>
+                                        </div>
+                                        <h3 class="card-title">Realizar carga</h3>
+                                    </div>
+                                    &nbsp;
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <a href="download.php">
+                                <div class="card card-stats">
+                                    &nbsp;
+                                    <div class="card-header card-header-primary card-header-icon">
+                                        <div class="card-icon">
+                                            <i class="fa fa-cloud-download" aria-hidden="true"></i>
+                                        </div>
+                                        <h3 class="card-title">Descargar Respaldo</h3>
+                                    </div>
+                                    &nbsp;
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="copyright float-center">
+                        &copy;
+                        <script>
+                            document.write(new Date().getFullYear())
+                        </script> made by
+                        <a href="#" target="_blank">Arturo Ruiz</a>.
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+</body>
+
+<script src="vendor/sweet-alert/sweetalert2.all.min.js"></script>
+
+<script>
+    Swal.fire(
+        'Subida Realizada Correctamente!',
+        'Sus archivos han sido respaldados en google drive!',
+        'success'
+    )
+</script>
+
+</html>
--- upload.php
+++ upload.php
@@ -1,53 +1,51 @@
 <?php
 
-include 'api-google/vendor/autoload.php';
+include 'api-google/vendor/autoload.php'; // Loads the Google API PHP Client Library.
 
 putenv('GOOGLE_APPLICATION_CREDENTIALS=respaldo-database-eabfd082c9ee.json');    // Path to service account credentials
 
-$client = new Google_Client();
+$client = new Google_Client(); // Create a Google client.
 
-$client->useApplicationDefaultCredentials();
+$client->useApplicationDefaultCredentials(); // Get credentials from the application default credentials.
 
-$client->setScopes(['https://www.googleapis.com/auth/drive.file']);
+$client->setScopes(['https://www.googleapis.com/auth/drive.file']); // Scopes required by the API.
 
 try {
     //code...
-    $service = new Google_Service_Drive($client);
+    $service = new Google_Service_Drive($client); // Create a Google Drive service instance.
 
-    $file_path = 'C:\xampp\htdocs\Angeluz-Backup-System\upload.txt';
+    $file_path = 'C:\xampp\htdocs\Angeluz-Backup-System\upload\test.txt'; // Path to the file to be uploaded
 
-    $file_name = 'test.txt';
+    $file_name = 'test.txt'; // File name to be uploaded
 
-    $file = new Google_Service_Drive_DriveFile();
+    $file = new Google_Service_Drive_DriveFile(); // Create a Google Drive file instance.
 
-    $result = $service->files->listFiles([
-        'q' => "name = '$file_name'",
-        'fields' => 'files(id, size)'
+    $result = $service->files->listFiles([  // List all files in the user's Drive.
+        'q' => "name = '$file_name'",       // Search for a file by name.
+        'fields' => 'files(id, size)'       // Only return the file ID and size.
     ]);
-    
-    $num = count($result);
-    
-    $fileId = $result[0]->id;
 
-    $service->files->delete($fileId);
+    $num = count($result); // Get the number of files in the user's Drive.
 
-    $file->setName($file_name);
-    
-    $file->setParents(array("1Ai92X0uuovpO8Rcfeg-eXnzr3B0K3357"));
+    $fileId = $result[0]->id; // Get the file ID.
+
+    $service->files->delete($fileId); // Delete the file
+
+    $file->setName($file_name);   // Set the file name.
+
+    $file->setParents(array("1Ai92X0uuovpO8Rcfeg-eXnzr3B0K3357")); // Parent folder id
 
 
-    $result = $service->files->create($file, array(
-        'data' => file_get_contents($file_path),
-        'uploadType' => 'multipart',
+    $result = $service->files->create($file, array( // Create a Google Drive file instance.
+        'data' => file_get_contents($file_path), // Set the file contents.
+        'uploadType' => 'multipart', // Use the resumable upload type.
     ));
 
-} catch (Google_Service_Exception $gs) {
+    header('Location: successful_upload.php'); // Redirect to the success page.
 
-    $mensaje = json_decode($gs->getMessage());
-    echo $mensaje->error->message();
-
+} catch (Google_Service_Exception $gs) { // Google Drive API error.
+    $msj = json_decode($gs->getMessage()); // Get the error message.
+    echo $msj->error->message(); // Print the error message.
 } catch (Exception $e) {
-
-    echo $e->getMessage();
-
+    echo $e->getMessage(); // Print the error message.
 }
